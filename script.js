fb3596808f32edf99c8cb97c1d9e3a45f37001f8
David Islam <david.islam24@gmail.com>
2023-09-12 13:34:49 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Returns a list of moves the knight will make to 
+get from square <start> to square <end>. */
+function knightMoves(start, end) {
+  // TODO
+}
+
+console.log(knightMoves([0, 0], [1,2]));
+console.log(knightMoves([0,0], [3,3]));
+console.log(knightMoves([3,3], [0,0]));
